035597d8524a41528d58f51938f0feab974d7e8e
Jeferson Farias Calazans <calazans10@gmail.com>
2013-11-30 19:00:10 -0200

    splitando modelos da app de cliente

---
+++ clients/models/__init__.py
@@ -0,0 +1,2 @@
+from .client import Client
+from .address import Address
---
+++ clients/models/client.py
@@ -0,0 +1,15 @@
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class Client(TimeStampedModel):
+    user = models.OneToOneField('auth.User', verbose_name="Usuário")
+    cpf = models.CharField('CPF', max_length=14)
+    born_on = models.DateField('Nascido em')
+    addresses = models.ManyToManyField('Address', verbose_name="Endereços")
+
+    class Meta:
+        app_label = 'clients'
+        db_table = 'client'
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
