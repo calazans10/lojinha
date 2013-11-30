0ee00beadf32042bf5e28b86a90e77cc2667e85d
Jeferson Farias Calazans <calazans10@gmail.com>
2013-11-30 18:53:46 -0200

    criando modelo de endereço e padronizando as migrações da app

--- clients/migrations/0001_initial.py
+++ clients/migrations/0001_initial.py
@@ -11,24 +11,54 @@ class Migration(SchemaMigration):
         # Adding model 'Client'
         db.create_table('client', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('created', self.gf('model_utils.fields.AutoCreatedField')(default=datetime.datetime.now)),
+            ('modified', self.gf('model_utils.fields.AutoLastModifiedField')(default=datetime.datetime.now)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(unique=True, to=orm['auth.User'])),
             ('cpf', self.gf('django.db.models.fields.CharField')(max_length=14)),
             ('born_on', self.gf('django.db.models.fields.DateField')()),
         ))
         db.send_create_signal('clients', ['Client'])
+
+        # Adding M2M table for field addresses on 'Client'
+        m2m_table_name = db.shorten_name('client_addresses')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('client', models.ForeignKey(orm['clients.client'], null=False)),
+            ('address', models.ForeignKey(orm['clients.address'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['client_id', 'address_id'])
+
+        # Adding model 'Address'
+        db.create_table('address', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('model_utils.fields.AutoCreatedField')(default=datetime.datetime.now)),
+            ('modified', self.gf('model_utils.fields.AutoLastModifiedField')(default=datetime.datetime.now)),
+            ('street', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('neighborhood', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('zipcode', self.gf('django.db.models.fields.CharField')(max_length=9)),
+            ('city', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('state', self.gf('django.db.models.fields.CharField')(max_length=100)),
+        ))
+        db.send_create_signal('clients', ['Address'])
 
 
     def backwards(self, orm):
         # Deleting model 'Client'
         db.delete_table('client')
 
+        # Removing M2M table for field addresses on 'Client'
+        db.delete_table(db.shorten_name('client_addresses'))
+
+        # Deleting model 'Address'
+        db.delete_table('address')
+
 
     models = {
         'auth.group': {
             'Meta': {'object_name': 'Group'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '80', 'unique': 'True'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'to': "orm['auth.Permission']", 'symmetrical': 'False'})
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'blank': 'True', 'to': "orm['auth.Permission']"})
         },
         'auth.permission': {
             'Meta': {'object_name': 'Permission', 'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)"},
@@ -40,28 +70,42 @@ class Migration(SchemaMigration):
         'auth.user': {
             'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'email': ('django.db.models.fields.EmailField', [], {'blank': 'True', 'max_length': '75'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'to': "orm['auth.Group']", 'symmetrical': 'False'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True', 'to': "orm['auth.Group']"}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'to': "orm['auth.Permission']", 'symmetrical': 'False'}),
-            'username': ('django.db.models.fields.CharField', [], {'max_length': '30', 'unique': 'True'})
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'user_set'", 'blank': 'True', 'to': "orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'clients.address': {
+            'Meta': {'object_name': 'Address', 'db_table': "'address'"},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'created': ('model_utils.fields.AutoCreatedField', [], {'default': 'datetime.datetime.now'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'}),
+            'neighborhood': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'state': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '9'})
         },
         'clients.client': {
-            'Meta': {'db_table': "'client'", 'object_name': 'Client'},
+            'Meta': {'object_name': 'Client', 'db_table': "'client'"},
+            'addresses': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': "orm['clients.Address']"}),
             'born_on': ('django.db.models.fields.DateField', [], {}),
             'cpf': ('django.db.models.fields.CharField', [], {'max_length': '14'}),
+            'created': ('model_utils.fields.AutoCreatedField', [], {'default': 'datetime.datetime.now'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+            'modified': ('model_utils.fields.AutoLastModifiedField', [], {'default': 'datetime.datetime.now'}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'unique': 'True', 'to': "orm['auth.User']"})
         },
         'contenttypes.contenttype': {
-            'Meta': {'db_table': "'django_content_type'", 'object_name': 'ContentType', 'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)"},
+            'Meta': {'object_name': 'ContentType', 'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
--- clients/models.py
+++ clients/models.py
@@ -1,13 +1,29 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
 
-class Client(models.Model):
-    user = models.OneToOneField('auth.User', related_name="client")
-    cpf = models.CharField(max_length=14)
-    born_on = models.DateField()
+class Client(TimeStampedModel):
+    user = models.OneToOneField('auth.User', verbose_name="Usuário")
+    cpf = models.CharField('CPF', max_length=14)
+    born_on = models.DateField('Nascido em')
+    addresses = models.ManyToManyField('Address', verbose_name="Endereços")
 
     class Meta:
         app_label = 'clients'
         db_table = 'client'
         verbose_name = 'Cliente'
         verbose_name_plural = 'Clientes'
+
+
+class Address(TimeStampedModel):
+    street = models.CharField('Rua', max_length=100)
+    neighborhood = models.CharField('Bairro', max_length=100)
+    zipcode = models.CharField('CEP', max_length=9)
+    city = models.CharField('Cidade', max_length=100)
+    state = models.CharField('Estado', max_length=100)
+
+    class Meta:
+        app_label = 'clients'
+        db_table = 'address'
+        verbose_name = 'Endereço'
+        verbose_name_plural = 'Endereços'
