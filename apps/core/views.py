47330007ec0b7a4cf1af6095c20de30867691a1c
Jeferson Farias Calazans <calazans10@gmail.com>
2013-12-10 10:56:35 -0200

    spliting template to organize project

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,21 +2,27 @@ from django.views.generic import TemplateView
 from products.models import Product, Category
 
 
-class IndexView(TemplateView):
+class BaseView(TemplateView):
+    def get_categories(self):
+        return Category.objects.all()
+
+    def get_base_context(self):
+        cxt = {}
+        cxt['nodes'] = self.get_categories()
+
+        return cxt
+
+
+class IndexView(BaseView):
     template_name = 'core/index.html'
 
     def get_products(self):
         return Product.objects.all().select_related('category')
 
-    def get_categories(self):
-        return Category.objects.all()
-
     def get(self, request, *args, **kwargs):
         products = self.get_products()
-        categories = self.get_categories()
 
-        cxt = {}
+        cxt = self.get_base_context()
         cxt['products'] = products
-        cxt['nodes'] = categories
 
         return self.render_to_response(cxt)
