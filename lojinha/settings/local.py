3f00b4191948e8bcb0962d680a0d5529534d0e73
Jeferson Farias Calazans <calazans10@gmail.com>
2013-12-16 23:31:58 -0200

    removing postgrespassword variable

--- lojinha/settings/local.py
+++ lojinha/settings/local.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-import os
 from .base import *
 
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-
-POSTGRESPASSWORD = os.environ.get('POSTGRESPASSWORD')
 
 DATABASES = {
     'default': {
