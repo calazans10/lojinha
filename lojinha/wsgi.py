1c63023b942f6a47f487ff49f7049375a3a3518e
Jeferson Farias Calazans <calazans10@gmail.com>
2013-12-25 15:55:56 -0200

    remove errors on wsgi

--- lojinha/wsgi.py
+++ lojinha/wsgi.py
@@ -1,9 +1,10 @@
 import os
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
 
 DJANGO_SETTINGS = os.environ.get('DJANGO_SETTINGS')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", DJANGO_SETTINGS)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'DJANGO_SETTINGS')
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 application = Cling(get_wsgi_application())
