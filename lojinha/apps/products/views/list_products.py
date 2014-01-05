f2f2830c7d594573e492a14e3748fd5dc6e8c3de
Jeferson Farias Calazans <calazans10@gmail.com>
2014-01-05 09:55:47 -0200

    Add best practices for django class based views

--- lojinha/apps/products/views/list_products.py
+++ lojinha/apps/products/views/list_products.py
@@ -6,15 +6,11 @@ from core.views import BaseView
 class ListProducts(BaseView):
     template_name = 'products/list_products.html'
 
-    def filter_products(self, category):
-        products = self.get_products().filter(category=category)
-        return products
-
     def get(self, request, object_id, *args, **kwargs):
         category = Category.objects.get(pk=object_id)
-        products = self.filter_products(category)
+        products = self.get_products().by_category(category)
 
-        cxt = self.get_base_context()
+        cxt = self.get_context_data()
         cxt['category'] = category
         cxt['products'] = products
 
